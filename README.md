11152575be243d8857f9f53a9f2eb85cbeee6c34
Jean Demel <demeljean@hotmail.fr>
2024-07-17 16:58:53 +0200

    crud complete

--- README.md
+++ README.md
@@ -11,8 +11,9 @@ Un projet symfony avec l'ORM doctrine
 ## Exercice
 ### Le contrôleur de l'Article
 1. Générer un contrôleur (`php bin/console ma:con`) ArticleController
-2. Rajouter un constructeur avec 2 argument private, un ArticleRepository $repo et un EntityManagerInterface $em
-3. Utiliser le $repo pour faire les 2 routes de GET sur /api/article et /api/article/{id}
-4. Utiliser le $em->persist() suivi d'un $em->flush() pour faire le POST (comme on l'avait fait dans le projet avec les chiens). Avant de persist, pourquoi pas assigner la date de maintenant au createdAt.
-5. Utiliser les mêmes méthodes du $em pour faire le PUT
-6. Utiliser le $em->find et le $em->remove suivi d'un $em->flush() pour faire la suppression
+2. Rajouter un constructeur avec 2 arguments private, un ArticleRepository $repo et un EntityManagerInterface $em
+3. Utiliser le $this->repo pour faire les 2 routes de GET sur /api/article et /api/article/{id}
+4. Utiliser le $this->em->persist() suivi d'un $this->em->flush() pour faire le POST (comme on l'avait fait dans le projet avec les chiens). Avant de persist, pourquoi pas assigner la date de maintenant au createdAt.
+5. Utiliser les mêmes méthodes du $this->em pour faire le PUT
+6. Utiliser le $this->em->find et le $this->em->remove suivi d'un $this->em->flush() pour faire la suppression
+7. Pour le plaisir, pourquoi pas faire de la validation : le title et le content ne doivent pas être vides, et on va dire que le title doit faire entre 3 et 100 caractères
---
+++ src/Controller/ArticleController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Article;
+use App\Repository\ArticleRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Attribute\MapRequestPayload;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Serializer\SerializerInterface;
+
+#[Route('/api/article')]
+class ArticleController extends AbstractController
+{
+
+    public function __construct(
+        private ArticleRepository $repo, 
+        private EntityManagerInterface $em
+    ){}
+
+    #[Route(methods: 'GET')] 
+    public function all(): JsonResponse
+    {
+        return $this->json(
+            $this->repo->findAll()
+        );
+    }
+    /**
+     * Ici Symfony et Doctrine travaillent main dans la main et juste en indiquant l'id
+     * en paramètre et le type d'entité à aller chercher et il feront le find automatiquement
+     * et une erreur 404 si jamais pas de résultat
+     */
+    #[Route('/{id}', methods:'GET')]
+    public function one(Article $article): JsonResponse {
+       
+        return $this->json($article);
+    }
+    /*
+        #[Route('/{id}', methods:'GET')]
+    public function one(int $id): JsonResponse {
+        
+        $article = $this->repo->find($id);
+        //Si on a pas trouvé l'article pour cet id, on renvoie une erreur 404
+        if(!$dog) {
+            throw new NotFoundHttpException("Article not found");
+            
+        }
+        return $this->json($article);
+    }
+        */
+    #[Route(methods:'POST')]
+    public function add(#[MapRequestPayload] Article $article): JsonResponse {
+        //On assigne la date de maintenant en createdAt
+        $article->setCreatedAt(new \DateTimeImmutable());
+        $this->em->persist($article);
+        $this->em->flush();
+        return $this->json($article, 201);
+    
+    }
+
+    #[Route('/{id}',methods:'DELETE')]
+    public function delete(Article $article):JsonResponse {
+        $this->em->remove($article);
+        $this->em->flush();
+        return $this->json(null, 204);
+    }
+
+    #[Route('/{id}', methods:'PUT')]
+    public function update(Article $article, Request $request, SerializerInterface $serializer) {
+        $serializer->deserialize($request->getContent(),Article::class,'json',
+        [
+            'object_to_populate' => $article
+        ]);
+        $this->em->flush();
+        return $this->json($article);
+    }
+}
